39dce6182ed1687f208d0cbc105d6282b553eba3
Saeid Mohadjer <saeid@fastmail.fm>
2023-12-07 12:06:43 +0100

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# JWT Authentication using Jose library and Fetch API (no cookie)
+
+On Vercel Edge runtime:
+https://jwt-js-saeid-fastmailfm.vercel.app/
+
+On Render:
+https://jwt-js.onrender.com/
+
+Run on localhost via npm:
+````
+npm run dev-express
+````
+
+Or if you have Vercel CLI installed:
+````
+vercel dev
+````
+
+
+
---
+++ api/authenticate.js
@@ -0,0 +1,19 @@
+import {SignJWT} from 'jose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default async (req, res) => {
+  const {username, password} = req.body;
+  const secret = new TextEncoder().encode(process.env.secret);
+
+  if (username === process.env.username && password === process.env.password) {
+    const token = await new SignJWT({ 'username': username })
+      .setProtectedHeader({ alg: 'HS256' })
+      .setExpirationTime('1m')
+      .sign(secret);
+    res.json({token});
+  } else {
+    res.status(403).end();
+  }
+}
